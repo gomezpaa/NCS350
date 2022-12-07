6e6984df3bb36bb51d3ea8bda9f20999deff59f6
gomezpaa <gomez.paolo@outlook.com>
2022-12-07 13:42:26 -0500

    This version attempts to forge probe requests using the MAC addresses saved in the Beacon tuple. The forged probe requests are being sent, but nothing is being collected in the new reveal function.

--- sniff.py
+++ sniff.py
@@ -7,20 +7,25 @@ from scapy.layers.dot11 import *
 
 Probe = {}
 Beacon = {}
+Hidden = {}
+Revealed = {}
 
 # Function that sniff packets async. and checks if the packet has a Dot11 layer
-# If true, we want to know if it is a Probe Response or a Beacon frame and extract the data
+# If true, we want to know if it is a Probe Response or a Beacon frame and extract the data            
+
 def sniff(p):
     rssi = p[RadioTap].dBm_AntSignal
 
     # Check if the captured packet has a Dot11 layer 
-    if p.haslayer(Dot11):
-
+    if not p.haslayer(Dot11):
+        return
+    else:
         # Check if the packet is a Probe Response
         if p.type == 0 and p.subtype == 5:
             frameType = "PROBE_RESP"
             if not p.info:
-                return
+                Probe[p.addr3] = ("Hidden SSID", ord(p[Dot11Elt:3].info), rssi, frameType)
+                Hidden[p.addr3] = ("Hidden SSID")
             else:
                 Probe[p.addr3] = (((p.info).decode('UTF-8')), ord(p[Dot11Elt:3].info), rssi, frameType)
         
@@ -35,8 +40,17 @@ def sniff(p):
                     Beacon[p.addr3] = ("Hidden SSID", ord(p[Dot11Elt:3].info), rssi, frameType)
                 else:
                     Beacon[p.addr3] = ((p.info).decode('UTF-8'), ord(p[Dot11Elt:3].info), rssi, frameType)
-                        
-                
+
+
+def reveal(p):
+    if not p.haslayer(Dot11):
+        return
+    else:
+        if p.type == 0 and p.subtype == 5:
+            if not p.info:
+                return
+            else:
+                Revealed[p.addr3] = ((p.info).decode('UTF-8'))
 
 def main():
     name = "AP Scanner"
@@ -54,10 +68,6 @@ def main():
     time.sleep(0.5)
     print("%s is in monitor mode" % interface)
     time.sleep(1)
-
-    packet = RadioTap()/Dot11(type=0,subtype=4,addr1="FF:FF:FF:FF:FF:FF", addr2="00:c0:ca:b1:24:92",addr3="FF:FF:FF:FF:FF:FF")/Dot11Elt(ID="SSID", info="")
-    sendp(packet, iface=interface, count=5)
-    print("Sent 5 Probe Request with the following info: ", str(packet))
 
     # Create a sniffer function 
     t = AsyncSniffer(prn=sniff, iface=interface, count=0)
@@ -77,26 +87,38 @@ def main():
     print(name.center(65, " "))
 
     #  Prints out the Beacon frame details
-    print("=================================================================================")
-    print("=  frameType  =           SSIDs           =  CH   =   RSSI  =     MAC Address   =")
-    print("=================================================================================")
+    print("================================================================================")
+    print("=  frameType  =           SSIDs           =  CH   =   RSSI  =    MAC Address   =")
+    print("================================================================================")
     for key in Beacon.keys():
         print("=", (Beacon[key][3]).center(10, ' '),"=", (str(Beacon[key][0])).center(25, ' '), "=", str(Beacon[key][1]).center(5, ' '), "=", Beacon[key][2], "dBm", "=", key,"=")
-    print("=================================================================================\n")
+    print("================================================================================\n")
 
     # Prints out the Probe Response frame details
-    print("=================================================================================")
-    print("=  frameType  =           SSIDs           =  CH   =   RSSI  =     MAC Address   =")
-    print("=================================================================================")
+    print("================================================================================")
+    print("=  frameType  =           SSIDs           =  CH   =   RSSI  =    MAC Address   =")
+    print("================================================================================")
     for key in Probe.keys():
         print("=", (Probe[key][3]).center(10, ' '),"=", (str(Probe[key][0])).center(25, ' '), "=", str(Probe[key][1]).center(5, ' '), "=", Probe[key][2], "dBm", "=", key,"=")
-    print("=================================================================================\n")
+    print("================================================================================\n")
 
     # Compares both tables to see if there is a common MAC address between the two 
+    
+    a = AsyncSniffer(prn=reveal, iface=interface, count=0)
+    a.start()
+
     for Beaconkey in Beacon.keys():
-        for Probekey in Probe.keys():
-            if Beaconkey == Probekey:
-                print("This SSID was orignally known as:", Beacon[Beaconkey][0], "  After Probe Request:", Probe[Probekey][0], "  MAC Address =", Probekey)
+        if Beacon[Beaconkey][0] == "Hidden SSID":
+                print("Forging a packet with the Transmitter Address set to:", Beaconkey)
+                packet = RadioTap()/Dot11(type=0,subtype=4,addr1="FF:FF:FF:FF:FF:FF", addr2=Beaconkey, addr3="ff:ff:ff:ff:ff:ff")/Dot11Elt(ID='SSID', info="")
+                sendp(packet, iface=interface, count=3)
+    
+    for revealedKey in Revealed.keys():
+        print("Found hidden SSID! The SSID is:", Revealed[revealedKey][0])
+       
+    input("Press ENTER to stop scanning and print results\n")
+
+    
 
 if __name__ == "__main__":
     main()
